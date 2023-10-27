b6e53a9bcfdfd22c889d94ead84c80e81d395027
bscarpari <brunoomoraes686@gmail.com>
2023-10-27 18:15:27 -0300

    chore: adding analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,18 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
+import vercel from '@astrojs/vercel/serverless';
 import { SITE_URL } from "./src/data/config";
 
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   integrations: [tailwind(), sitemap(), robotsTxt()],
   site: SITE_URL,
   markdown: {
